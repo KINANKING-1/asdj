10b68bd6435346a048bf490c7f7fc0b3e42b81e2
KINANKING-1 <42686375+KINANKING-1@users.noreply.github.com>
2019-05-18 18:22:45 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 var prefix = "-";
 var adminprefix = '-'
 
-const developers = ["510854979364585475","id"]
+const developers = ["568478389183447080","id"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
